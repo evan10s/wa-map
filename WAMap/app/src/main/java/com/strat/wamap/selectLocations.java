9d01a472ae63fd70bae902b59b67334b72154a47
Evan Strat <evan10s@users.noreply.github.com>
2016-08-27 14:08:37 -0400

    Add submit button, start testing how to get selected locations

--- WAMap/app/src/main/java/com/strat/wamap/selectLocations.java
+++ WAMap/app/src/main/java/com/strat/wamap/selectLocations.java
@@ -3,6 +3,8 @@ package com.strat.wamap;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -16,6 +18,23 @@ public class selectLocations extends AppCompatActivity {
 
         setupSimpleSpinner(this, R.id.start_loc, R.array.locations);
         setupSimpleSpinner(this, R.id.end_loc, R.array.locations);
+    }
+
+    /*
+    * Returns the index of the current selected item in a spinner
+    * @param spinner The spinner to get the selected item for
+    * @return The index of the selected item in the referenced spinner
+     */
+    private int getSelectedSpinnerItem(int spinner) {
+        Spinner resultSpinner = (Spinner) findViewById(spinner);
+        return resultSpinner.getSelectedItemPosition();
+    }
+
+    public void checkResponses(View view) {
+        Log.i("select_locations","checkResponses was called");
+        Log.i("select_locations","Selected item: " + getSelectedSpinnerItem(R.id.start_loc));
+        Log.i("select_locations","Selected item: " + getSelectedSpinnerItem(R.id.end_loc));
+
     }
 
     private void setupSimpleSpinner(Context context, int spinnerId, int spinnerChoicesRes) {
