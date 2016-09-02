00fe0ed88f3059e3545f0612b49d5702a41f7b63
Evan Strat <evan10s@users.noreply.github.com>
2016-09-01 20:01:06 -0400

    Start adding some unit tests

---
+++ WAMap/app/src/test/java/com/strat/wamap/LocationTest.java
@@ -0,0 +1,28 @@
+package com.strat.wamap;
+
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LocationTest {
+
+    @Test
+    public void LocationCreateTest() throws Exception {
+        Location loc = new Location ("one",10,15);
+        Assert.assertTrue(loc.x == 10);
+        Assert.assertTrue(loc.y == 15);
+        Assert.assertTrue(loc.name.equals("one"));
+    }
+
+    @Test
+    public void LocationMidpointTest() throws Exception {
+        Location start = new Location("1",10,10);
+        Location end = new Location("2",20,20);
+        Path path = new Path(start, end);
+        path.getMidPoint();
+        Assert.assertTrue(path.getMidPoint().x==15);
+        Assert.assertTrue(path.getMidPoint().y==15);
+    }
+}
